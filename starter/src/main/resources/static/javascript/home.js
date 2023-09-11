6c876948505d1fbbed01cda72a056c2a826e3517
sornya <sornyaa9@hotmail.com>
2023-09-11 12:18:44 -0400

    edit login.js

--- starter/src/main/resources/static/javascript/home.js
+++ starter/src/main/resources/static/javascript/home.js
@@ -1,100 +1,88 @@
-// a logout method that clears that cookie
-const cookieArr = document.cookie.split("=");
+//Cookie
+const cookieArr = document.cookie.split("=")
 const userId = cookieArr[1];
 
-//Dom Elements
+//DOM Elements
+const submitForm = document.getElementById("note-form")
+const noteContainer = document.getElementById("note-container")
 
-const submitForm = document.getElementById("note-form");
-const noteContainer = document.getElementById("note-container");
+//Modal Elements
+let noteBody = document.getElementById(`note-body`)
+let updateNoteBtn = document.getElementById('update-note-button')
 
-//modal elements
-
-let noteBody = document.getElementById("note-body");
-let updateNoteBtn = document.getElementById("update-note-button");
-
-const headers ={
- 'Content-Type':'application/json'
+const headers = {
+    'Content-Type': 'application/json'
 }
 
 const baseUrl = "http://localhost:8080/api/v1/notes/"
 
-// form that submits new notes
-const handleSubmit = async (e)=>{
-   e.preventDefault()
-   let bodyObj ={
-    body:document.getElementById("note-input").value
-   }
-   await addNote(bodyObj);
-   document.getElementById('note-input').value = '' //clear input
- }
-
-//addNote method
- async function addNote(obj){
-   const response = await fetch(`${baseUrl}user/${userId}`,{
-    method:"POST",
-    body:JSON.stringify(obj),
-    headers: headers
-   })
-   .catch(err => console.error(err.message))
-   if(response.status == 200){
-   return getNotes(userId);
-   }
- }
-
-//getNotes method to retrieve all the notes that are associated with the user when the page loads, create cards for them, and append them to a container to hold them
-async function getNotes(userId){
- await fetch(`${baseUrl}user/${userId}`,{
-  method:"GET",
-  headers:headers
- })
- .then(response => response.json())
- .then(data => createNoteCards(data))
- .catch(err => console.error(err))
+const handleSubmit = async (e) => {
+    e.preventDefault()
+    let bodyObj = {
+        body: document.getElementById("note-input").value
+    }
+    await addNote(bodyObj);
+    document.getElementById("note-input").value = ''
 }
 
-//to be able to update a note which will involve a separate GET request for just that note so we can populate our modal with it
-async function getNoteById(noteId){
-  await fetch(`${baseUrl}/${noteId}`, {
-  method: "GET",
-  headers:headers
-  })
-  .then(res => res.json())
-  .then(data => populateModal(data))
-  .catch(err => console.error(err.message))
-}
-async function handleNoteEdit(noteId){
-  let bodyObj ={
-       id:noteId,
-       body: noteBody.value
-  }
-  await fetch(`${baseUrl}`,{
-       method: "PUT",
-       body:JSON.stringify(bodyObj),
-       headers:headers
-  })
-  .catch(err => console.error(err))
-  return getNotes(userId)
-}
-
-//handleDelete method to be able to delete a note
-async function handleDelete(noteId){
-    await fetch(`${baseUrl}/${noteId}`,{
-    method: "DELETE",
-    headers:headers
+async function addNote(obj) {
+    const response = await fetch(`${baseUrl}user/${userId}`, {
+        method: "POST",
+        body: JSON.stringify(obj),
+        headers: headers
     })
-    .catch(err => console.error(err))
-    return getNotes(userId)
+        .catch(err => console.error(err.message))
+    if (response.status == 200) {
+        return getNotes(userId);
+    }
 }
 
-//a logout method that clears that cookie
-function handleLogout(){
-  let c = document.cookie.split(";");
-  for(let i in c){
-  document.cookie = /^[^=]+/.exec(c[i])[0]+'=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
-  }
+async function getNotes(userId) {
+    await fetch(`${baseUrl}user/${userId}`, {
+        method: "GET",
+        headers: headers
+    })
+        .then(response => response.json())
+        .then(data => createNoteCards(data))
+        .catch(err => console.error(err))
 }
 
-//These methods utilized some helper functions that we made as well, the first you’ll see is called “createNoteCards” and it accepts an array of objects. It then loops through the array and creates a little note card for each item and appends it to our container for the notes.
+async function handleDelete(noteId){
+    await fetch(baseUrl + noteId, {
+        method: "DELETE",
+        headers: headers
+    })
+        .catch(err => console.error(err))
+
+    return getNotes(userId);
+}
+
+async function getNoteById(noteId){
+    await fetch(baseUrl + noteId, {
+        method: "GET",
+        headers: headers
+    })
+        .then(res => res.json())
+        .then(data => populateModal(data))
+        .catch(err => console.error(err.message))
+}
+
+async function handleNoteEdit(noteId){
+    let bodyObj = {
+        id: noteId,
+        body: noteBody.value
+    }
+
+    await fetch(baseUrl, {
+        method: "PUT",
+        body: JSON.stringify(bodyObj),
+        headers: headers
+    })
+        .catch(err => console.error(err))
+
+    return getNotes(userId);
+}
+
 const createNoteCards = (array) => {
     noteContainer.innerHTML = ''
     array.forEach(obj => {
@@ -117,23 +105,24 @@ const createNoteCards = (array) => {
         noteContainer.append(noteCard);
     })
 }
+function handleLogout(){
+    let c = document.cookie.split(";");
+    for(let i in c){
+        document.cookie = /^[^=]+/.exec(c[i])[0]+"=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    }
+}
 
-// “populateModal” method which accepts an object as an argument and uses that object to populate the fields within the modal as well as assign a custom “data-” tag to the “Save” button element
-const populateModal = obj =>{
+const populateModal = (obj) =>{
     noteBody.innerText = ''
     noteBody.innerText = obj.body
     updateNoteBtn.setAttribute('data-note-id', obj.id)
 }
 
-//invoke methods
-
-getNote(userId)
+getNotes(userId);
 
 submitForm.addEventListener("submit", handleSubmit)
 
-updateNoteBtn.addEventListener("click", e=>{
-  let noteId = e.target.getAttribute('data-note-id');
-  handleNoteEdit(noteId);
+updateNoteBtn.addEventListener("click", (e)=>{
+    let noteId = e.target.getAttribute('data-note-id')
+    handleNoteEdit(noteId);
 })
-
-<a class="btn btn-danger navbar-btn" href="./login.html" onclick="handleLogout()">Logout</a>
--- starter/src/main/resources/static/javascript/register.js
+++ starter/src/main/resources/static/javascript/register.js
@@ -4,29 +4,33 @@ const registerForm = document.getElementById('register-form');
 const registerUsername = document.getElementById('register-username');
 const registerPassword = document.getElementById('register-password');
 
-// we have a base set of headers that will be included with every request.
-const headers ={
-'Content-Type':'application/json'
+const headers = {
+    'Content-Type':'application/json'
 }
 
 const baseUrl = 'http://localhost:8080/api/v1/users'
-// create a function that will handle Submitting the form.
 
-const handleSubmit = async(e)=>{
-   e.preventDefault()
-   let bodyObj = {
-       username : registerUsername.value,
-       password: registerPassword.value
-   }
-   const response = await fetch(`${baseUrl}/register`,{
-       method:"POST",
-       body: JSON.stringify(bodyObj) //convert javascript value to JSON string
-       headers: headers
-   })
-   .catch(err=> console.err(err.message))
-   const responseArr = await response.json()
-   if(response.status === 200){
-   window.location.replace(responseArr[0])
-   }
+
+const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+    let bodyObj = {
+        username: registerUsername.value,
+        password: registerPassword.value
+    }
+
+    const response = await fetch(`${baseUrl}/register`, {
+        method: "POST",
+        body: JSON.stringify(bodyObj),
+        headers: headers
+    })
+        .catch(err => console.error(err.message))
+
+    const responseArr = await response.json()
+
+    if (response.status === 200){
+        window.location.replace(responseArr[0])
+    }
 }
-registerForm.addEventListener("submit", handleSubmit);
+
+registerForm.addEventListener("submit", handleSubmit)
